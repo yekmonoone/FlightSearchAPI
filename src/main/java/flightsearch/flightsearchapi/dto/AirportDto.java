97f05f4a15dcf0ff261352ba045b49239a4ffa24
ebhazarcodes <erdoganbaranhazar@gmail.com>
2024-01-14 15:46:27 +0300

    add AirportDto

---
+++ src/main/java/flightsearch/flightsearchapi/dto/AirportDto.java
@@ -0,0 +1,17 @@
+package flightsearch.flightsearchapi.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class AirportDto {
+
+    private Long id;
+    private String city;
+
+}
