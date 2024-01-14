166d007d9ee1462e8ba33867875d67c2ffe1f5b0
ebhazarcodes <erdoganbaranhazar@gmail.com>
2024-01-14 15:46:37 +0300

    add FlightDto

--- src/main/java/flightsearch/flightsearchapi/dto/FlightDto.java
+++ src/main/java/flightsearch/flightsearchapi/dto/FlightDto.java
@@ -1,4 +1,24 @@
 package flightsearch.flightsearchapi.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class FlightDto {
+
+    private Long id;
+    private String departureAirport;
+    private String arrivalAirport;
+    private Date departureDate;
+    private Date arrivalDate;
+    private double price;
+
 }
