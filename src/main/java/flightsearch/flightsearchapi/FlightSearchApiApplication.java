8b25fb0a8e1a23ba775341943633fd5a49ed18ac
ebhazarcodes <erdoganbaranhazar@gmail.com>
2024-01-14 23:41:59 +0300

    change FlightSearchApiApplication

--- src/main/java/flightsearch/flightsearchapi/FlightSearchApiApplication.java
+++ src/main/java/flightsearch/flightsearchapi/FlightSearchApiApplication.java
@@ -2,6 +2,7 @@ package flightsearch.flightsearchapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication(scanBasePackages = {"flightsearch.flightsearchapi"})
 public class FlightSearchApiApplication {
@@ -9,5 +10,6 @@ public class FlightSearchApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FlightSearchApiApplication.class, args);
 	}
+
 
 }
